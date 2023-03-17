2626a572f155a770921f155bc8b97caa0e037e53
Kevin <ly1@ualberta.ca>
2023-03-16 22:41:41 -0600

    add shooter and ability to move left and right

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const squares = document.querySelectorAll('.grid div');
   const resultDisplayer = document.querySelector('#result');
-  let width = 30;
+  let width = 15;
   let currentShooterIndex = 202;
   let currentInvaderIndex = 0;
   let alienInvadersTakenDown = [];
@@ -12,11 +12,36 @@ document.addEventListener('DOMContentLoaded', () => {
   // define alien invaders
   const alienInvaders = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    16, 17, 18, 19, 20, 21, 22, 23,
     30, 31, 32, 33, 34, 35, 36, 37, 38, 39
   ];
 
   // draw the alien invaders
   alienInvaders.forEach(invader => squares[currentInvaderIndex + invader].classList.add('invader'));
+
+  // draw the shooter
+  squares[currentShooterIndex].classList.add('shooter');
+
+  // move the shooter left and right
+  function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+    switch (e.keyCode) {
+      // left if divisible by 15
+      case 37:
+        if (currentShooterIndex % width !== 0) {
+          currentShooterIndex -= 1;
+        };
+        break;
+      // right if divible by 15 but less than 14
+      case 39:
+        if (currentShooterIndex % width < width - 1) {
+          currentShooterIndex += 1;
+        };
+        break;
+      };
+      squares[currentShooterIndex].classList.add('shooter');
+  };
+
+  document.addEventListener('keydown', moveShooter);
 
 });
